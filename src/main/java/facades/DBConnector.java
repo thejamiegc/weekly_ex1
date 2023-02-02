bd517e9ac86a107d41bf8f9d31d87e19a0d12ef0
Thomas Hartmann <thomas@webtrade.dk>
2023-02-02 13:01:58 +0100

    changed db credentials to be read in production

--- src/main/java/facades/DBConnector.java
+++ src/main/java/facades/DBConnector.java
@@ -1,8 +1,11 @@
 package facades;
 
+import dtos.DBUserDTO;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnector {
     private static String URL = "jdbc:mysql://localhost:3306/";
@@ -18,10 +21,20 @@ public class DBConnector {
     public static Connection connection(String db) throws ClassNotFoundException, SQLException {
         if ( singleton == null ) {
             Class.forName( "com.mysql.cj.jdbc.Driver" );
-            singleton = DriverManager.getConnection( URL+db, USER, PW );
+            DBUserDTO dbUserDTO = getCredentails();
+            singleton = DriverManager.getConnection( URL+db, dbUserDTO.getUserName(), dbUserDTO.getPassword() );
         }
         return singleton;
     }
+    private static DBUserDTO getCredentails() {
+        boolean isDeployed = (System.getenv("DEPLOYED") != null);
+        if (isDeployed) {
+            String user = System.getenv("USER");
+            String pw = System.getenv("PW");
+            return new DBUserDTO(user, pw);
+        }
+        return new DBUserDTO(USER, PW);
+        }
 
 }
 
